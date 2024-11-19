1d43df973828ec39d625f4c076ce53a7c9829e7b
Emma Haggren <emma.haggren.5261@student.lu.se>
2024-11-19 15:40:56 +0100

    Basic log in program done

---
+++ login/src/users.js
@@ -0,0 +1,7 @@
+const users = [
+  { username: "admin", password: "12345" },
+  { username: "user1", password: "password1" },
+  { username: "user2", password: "password2" },
+];
+
+export default users;
